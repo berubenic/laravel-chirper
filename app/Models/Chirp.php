bbf3604652e9db8713d10709bba7b78cb42f28ef
Nicholas <berubenic@gmail.com>
2024-12-21 23:30:56 -0500

    add mail notification for created chirp

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,18 +2,23 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\Facades\Date;
 
 /**
  * @property User $user
  * @property string $message
+ * @property mixed $user_id
  */
 class Chirp extends Model
 {
     protected $fillable = [
         'message'
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
 
     public function user(): BelongsTo
